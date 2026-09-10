2262126839385346a30e68e06a6e8cf97b87dc23
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-09-09 21:58:31 -0300

    feat : Reestruturando as rotas

---
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -0,0 +1,11 @@
+
+export default function EditarProdutos() {
+
+  
+
+  return (
+    <main>
+          <h2>Editar Produtos</h2>
+    </main>
+  )
+}
---
+++ my-app/src/routes/Produtos/index.tsx
@@ -0,0 +1,11 @@
+
+export default function Produtos() {
+    //Modificar o título da página;
+    document.title = "Produtos";
+    
+    return (
+        <main>
+            <h2>Produtos</h2>
+        </main>
+    )
+}
