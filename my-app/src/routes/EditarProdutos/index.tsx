0115a4009050930ed76cd5f8aa7cf06641085ae2
alexandreteixeiras <teixeiradossantosalexandre@gmail.com>
2026-09-09 22:50:49 -0300

    feat: userParams

--- my-app/src/routes/EditarProdutos/index.tsx
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -1,11 +1,34 @@
+import { useParams } from "react-router";
+const listaProdutos[
+  
+]
 
 export default function EditarProdutos() {
+  document.title = "Editar Produtos";
 
+  const { id } = useParams<string>();
+
+  const produto = listaProdutos.find(( p )=> p.id === Number(id) );
   
 
   return (
     <main>
           <h2>Editar Produtos</h2>
+
+          <div>
+            {produto ?(
+              <div>
+                <p>Nome do Produto:{produto.nome}</p>
+                <p>R$ :{produto.preco}</p>
+              </div>
+            ):
+            (
+              <p>Produto não encontrado!</p>
+            )
+          }
+
+          </div>
+          
     </main>
   )
 }
