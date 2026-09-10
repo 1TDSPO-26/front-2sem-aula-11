955c63203161cdfe52bc30eaef86b31652e546c1
borbagustavo <gustavo.borba.27@gmail.com>
2026-09-09 22:49:26 -0300

    feat: utilizando useParams.... desestruturando um objeto

--- my-app/src/components/Menu/Menu.tsx
+++ my-app/src/components/Menu/Menu.tsx
@@ -7,7 +7,7 @@ export default function Menu() {
         <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/produtos">Produtos</Link></li>
-            <li><Link to="/editar-produtos/:id">Editar Produtos</Link></li>
+            <li><Link to="/editar-produtos">Editar Produtos</Link></li>
         </ul>
     </nav>
   )
--- my-app/src/routes/EditarProdutos/index.tsx
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -1,10 +1,50 @@
 
+import { useParams } from "react-router"
 
-export default function EditarProdutos
-() {
+
+const listaProdutos = [
+  { id: 1, nome: "Produto 1", preco: 10.0 },
+  { id: 2, nome: "Produto 2", preco: 20.0 },
+  { id: 3, nome: "Produto 3", preco: 30.0 },
+];
+
+export default function EditarProdutos() {
+
+
+
+  document.title = "Editar Produtos"
+  const { id } = useParams<string>()
+  /*
+  //Uso do destructuring 
+  const estojo = {lapis: "Preto", caneta: "Azul", borracha: "Branca"}
+  
+  // aplicado o mesmo, para utilizar os atributos dos objetos.
+  const {lapis, caneta} = estojo;
+  
+  const jogos = ["Sonic", "Mario", "Zelda"]
+  
+  const [sonic, mario] = jogos  // ele pega posição do vetor, sendo assim [0] = sonic, [1] = mario, [2] = zelda
+  
+  */
+
+  const produto = listaProdutos.find((p) => p.id === Number(id));
   return (
-  <main>
-   
-  </main>
+    <main>
+      <div>
+
+
+
+        {produto ? (
+          <div>
+            <p>Nome: {produto.nome}</p>
+            <p>Preço: {produto.preco}</p>
+          </div>
+        ) :
+          (<p>Produto não encontrado</p>)
+        }
+
+      </div>
+
+    </main>
   )
 }
